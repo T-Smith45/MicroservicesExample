fadc564ce768eb58bb7ba3b51905a7933824f7db
T Smith <87288866+T-Smith45@users.noreply.github.com>
2025-08-31 19:32:46 -0500

    .NET Microservices parts 2 & 3

---
+++ PlatformService/Dtos/PlatformCreateDto.cs
@@ -0,0 +1,8 @@
+namespace PlatformService.Dtos;
+
+public record PlatformCreateDto
+{
+    public required string Name { get; set; }
+    public required string Publisher { get; set; }
+    public required string Cost { get; set; }
+}
